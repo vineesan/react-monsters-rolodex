06fc91dd8543adb8408ce8c63fb3cd8d3a58613c
Vineeth <vinith.vini@gmail.com>
2020-02-21 16:55:08 +0530

    Updates

---
+++ precache-manifest.d7d82e624b18e929af8a4f7d63040ed0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a8d6592200ff21a9f8fb02a29b54b82",
+    "url": "/vineesan/react-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fc920ac5396a6fb74cff",
+    "url": "/vineesan/react-monsters-rolodex/static/css/main.15bbbea0.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/vineesan/react-monsters-rolodex/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/vineesan/react-monsters-rolodex/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc920ac5396a6fb74cff",
+    "url": "/vineesan/react-monsters-rolodex/static/js/main.3590aa28.chunk.js"
+  },
+  {
+    "revision": "e042856ab13ea7afe2a9",
+    "url": "/vineesan/react-monsters-rolodex/static/js/runtime-main.5a5bbcd8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/vineesan/react-monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
